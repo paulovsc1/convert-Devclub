39594dfc8b3f9a2d4c6f22159f4cda9c7e71414c
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2025-05-18 15:49:42 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues () {
+    const inputCurrencyValue = document.querySelector(".input-currency").value 
+
+    const dolarToday = 5.66
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
+}
+
+
+
+
+convertButton.addEventListener("click", convertValues)
+
