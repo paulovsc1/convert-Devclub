cc325d16c91ae53cfaf6eae16d58b500d9661822
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2025-05-18 17:31:54 -0300

    fazendo um conversor de moedas usando HTML, CSS e JavaScript

--- scripts.js
+++ scripts.js
@@ -3,11 +3,24 @@ const convertButton = document.querySelector(".convert-button")
 function convertValues () {
     const inputCurrencyValue = document.querySelector(".input-currency").value 
 
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real
+
+    const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas
+
     const dolarToday = 5.66
 
     const convertedValue = inputCurrencyValue / dolarToday
 
-    console.log(convertedValue)
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency", 
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency", 
+        currency: "USD"
+    }).format(convertedValue)
+
 }
 
 
