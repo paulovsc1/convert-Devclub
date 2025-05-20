8ed24753d154cd65234a2ea77311df9676a979ca
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2025-05-19 21:21:15 -0300

    atualizei minha página de converter valores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const convertButton = document.querySelector(".convert-button")
 
+const currencySelect = document.querySelector(".currency-select")
+
 function convertValues () {
     const inputCurrencyValue = document.querySelector(".input-currency").value 
 
@@ -7,24 +9,40 @@ function convertValues () {
 
     const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas
 
+    console.log(currencySelect.value)
+
     const dolarToday = 5.66
+    const euroToday = 6.34
 
-    const convertedValue = inputCurrencyValue / dolarToday
-
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    // Sempre mostra o valor original em reais
+     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency", 
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    // Converte o valor com base na moeda selecionada
+    if(currencySelect.value == "dolar") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
         style: "currency", 
         currency: "USD"
-    }).format(convertedValue)
+    }).format(inputCurrencyValue/dolarToday)
+    }
+
+    if(currencySelect.value == "euro"){
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+        style: "currency", 
+        currency: "EUR"
+    }).format(inputCurrencyValue/euroToday)
+
+    }
+
+   
+    
 
 }
 
+convertButton.addEventListener("click", convertValues);
 
 
 
-convertButton.addEventListener("click", convertValues)
 
